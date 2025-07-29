df3232c382bca8f14c8899d89280881f202d9309
BurakSahin00 <122110899+BurakSahin00@users.noreply.github.com>
2025-07-29 16:48:58 +0300

    Login Page Created

--- src/app/navigation/GetStarted/page.tsx
+++ src/app/navigation/GetStarted/page.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 const GetStarted: React.FC = () => {
   return (
     <div>
-      GetStarted Page Content
+      <div className="">
+        <h2>Login and Start</h2>
+        <p>Login to your account to start using our services.</p>
+      </div>
+      <div className="">
+        <label htmlFor="email">Email Address</label>
+        <input type="email" name="Email" id="email" placeholder="Email" required />
+      </div>
+      <div className="">
+        <label htmlFor="password">Password</label>
+        <input type="password" name="Password" id="password" placeholder='Password' required/>
+      </div>
+      <input type="button" value="Login"/>
     </div>
   )
 }
