8a555233d758eb49c7f4be927735733d4e3e1471
BurakSahin00 <122110899+BurakSahin00@users.noreply.github.com>
2025-08-02 13:16:58 +0300

    Login Page Upgraded

--- src/app/navigation/GetStarted/page.tsx
+++ src/app/navigation/GetStarted/page.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import styles from './page.module.css'
 
 const GetStarted: React.FC = () => {
   return (
-    <div>
-      <div className="">
+    <div className={styles.master}>
+      <div className={styles.container}>
+      <div className={styles.mainText}>
         <h2>Login and Start</h2>
         <p>Login to your account to start using our services.</p>
       </div>
-      <div className="">
+      <div className={styles.inputLabel}>
         <label htmlFor="email">Email Address</label>
-        <input type="email" name="Email" id="email" placeholder="Email" required />
+        <input className={styles.inputBox} type="email" name="Email" id="email" placeholder="Email" required />
       </div>
-      <div className="">
+      <div className={styles.inputLabel}>
         <label htmlFor="password">Password</label>
-        <input type="password" name="Password" id="password" placeholder='Password' required/>
+        <input className={styles.inputBox} type="password" name="Password" id="password" placeholder='Password' required/>
       </div>
       <input type="button" value="Login"/>
+    </div>
     </div>
   )
 }
