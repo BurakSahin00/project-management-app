2bed570a9c208c7756ced72fcf8a48b6615ac86f
BurakSahin00 <122110899+BurakSahin00@users.noreply.github.com>
2025-08-02 14:08:35 +0300

    Login Box Finished

--- src/app/navigation/GetStarted/page.tsx
+++ src/app/navigation/GetStarted/page.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import styles from './page.module.css'
+import { IoMail } from "react-icons/io5";
+import { RiLockPasswordFill } from "react-icons/ri";
+
+
 
 const GetStarted: React.FC = () => {
   return (
     <div className={styles.master}>
       <div className={styles.container}>
-      <div className={styles.mainText}>
-        <h2>Login and Start</h2>
-        <p>Login to your account to start using our services.</p>
+        <div className={styles.mainText}>
+          <h2>Login and Start</h2>
+          <p>Login to your account to start using our services.</p>
+        </div>
+        <div className={styles.inputLabel}>
+          <IoMail className={styles.icon} />
+          <input className={styles.inputBox} type="email" name="Email" id="email" placeholder="Email" required />
+        </div>
+        <div className={styles.inputLabel}>
+          <RiLockPasswordFill className={styles.icon} />
+          <input className={styles.inputBox} type="password" name="Password" id="password" placeholder='Password' required />
+        </div>
+        <input className={styles.button} type="button" value="Login" />
       </div>
-      <div className={styles.inputLabel}>
-        <label htmlFor="email">Email Address</label>
-        <input className={styles.inputBox} type="email" name="Email" id="email" placeholder="Email" required />
-      </div>
-      <div className={styles.inputLabel}>
-        <label htmlFor="password">Password</label>
-        <input className={styles.inputBox} type="password" name="Password" id="password" placeholder='Password' required/>
-      </div>
-      <input type="button" value="Login"/>
-    </div>
     </div>
   )
 }
--- src/app/navigation/layout.tsx
+++ src/app/navigation/layout.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import React from "react";
+import { usePathname } from "next/navigation";
 import WelcomeHeader from "../components/welcome-header/WelcomeHeader";
 
 const WelcomeLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+
+  const pathname = usePathname();
   return (
     <div>
-      <WelcomeHeader />
+      {!(pathname === "/navigation/GetStarted") && <WelcomeHeader />}
       {children}
     </div>
   );
